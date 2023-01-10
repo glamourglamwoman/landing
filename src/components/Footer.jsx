899f8c44a9a88b9532cfd2af9f1a98f6d77af38a
torradopablo <torradopablo1@gmail.com>
2023-01-10 17:52:29 -0300

    i

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,45 @@
 import React from "react";
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBIcon,
+  MDBBtn
+} from 'mdb-react-ui-kit';
+
+import './components.css';
+
 
 const Footer = () => {
   return (
-    <>
-      <footer className="mb-0 text-center">
-        <div className="d-flex align-items-center justify-content-center pb-5">
-          <div className="col-md-6">
-            <p className="mb-3 mb-md-0">Made by Glamour Glam Woman
-            </p>
-          </div>
-        </div>
-      </footer>
-    </>
+        <MDBFooter className='bg-dark text-center text-white'>
+      <MDBContainer className='p-4 pb-0'>
+        <section className='mb-4'>
+          <MDBBtn outline color="light" floating className='m-1 footer-social-network-icon' href='#!' role='button'>
+            <MDBIcon fab icon='facebook-f' />
+          </MDBBtn>
+
+          <MDBBtn outline color="light" floating className='m-1 footer-social-network-icon' href='#!' role='button'>
+            <MDBIcon fab icon='instagram' />
+          </MDBBtn>
+
+          <MDBBtn outline color="light" floating className='m-1 footer-social-network-icon' href='#!' role='button'>
+            <MDBIcon fab icon='tiktok' />
+          </MDBBtn>
+
+
+        </section>
+      </MDBContainer>
+
+      <div className='text-center p-3 footer-copyright' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2022 Copyright:
+        <a className='text-white footer-brand' href='https://glamourglamwoman.github.io/landing/'>
+          Glamour Glam Woman
+        </a>
+      </div>
+    </MDBFooter>
   );
 };
 
 export default Footer;
+
+
