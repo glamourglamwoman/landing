65c9b695e9a04cba79bbe748bfc983b3cec6cf75
torradopablo <torradopablo1@gmail.com>
2023-01-16 16:15:07 -0300

    remove tiktok

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,15 +10,13 @@ import './components.css';
 
 //facebook-f
 //instagram
+//tiktok
 
 const Footer = () => {
   return (
         <MDBFooter className='bg-dark text-center text-white'>
       <MDBContainer className='p-4 pb-0'>
         <section className='mb-4'>
-          <MDBBtn outline color="light" target="blank" floating className='m-1 footer-social-network-icon' href='https://www.tiktok.com/@glamourglamw' role='button'>
-              <MDBIcon fab icon='tiktok' />
-          </MDBBtn>     
           <MDBBtn outline color="light" target="blank" floating className='m-1 footer-social-network-icon' href='https://www.instagram.com/glamour.glam.woman' role='button'>
               <MDBIcon fab icon='instagram' />
           </MDBBtn>  
